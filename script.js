9584b294241fd7cb33ab62206085414e60695ad7
alex <132571895+belcouact@users.noreply.github.com>
2025-03-19 22:24:46 +0800

    1

--- script.js
+++ script.js
@@ -4371,8 +4371,8 @@ function getSimplifiedContextSummary() {
         // Get poetry type and style
         // const poetryType = poetryTypeSelect ? poetryTypeSelect.value : '唐诗';
         // const poetryStyle = poetryStyleSelect ? poetryStyleSelect.value : '山水';
-        const poetryType = document.getElementById('poetry-type').value;
-        const poetryStyle = document.getElementById('poetry-style').value;
+        const poetryType = document.getElementById('poetry-type-select').value;
+        const poetryStyle = document.getElementById('poetry-style-select').value;
 
         console.log(`Generating poems for: ${school} ${grade}, Type: ${poetryType}, Style: ${poetryStyle}`);
         
