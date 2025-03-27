4d3eb426997258c82f964bd459d7769c1339d240
alex <132571895+belcouact@users.noreply.github.com>
2025-03-27 09:23:42 +0800

    1

--- script.js
+++ script.js
@@ -3731,7 +3731,7 @@ function getSimplifiedContextSummary() {
         setupNavigationButtons();
         
         // Poetry buttons
-        setupPoetryButtons();
+        // setupPoetryButtons();
         
         // Button click handlers for sidebar navigation
         if (qaButton) {
