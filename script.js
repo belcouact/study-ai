1c5972dd3ce92801deecb8dd47fa85e88050603e
alex <132571895+belcouact@users.noreply.github.com>
2025-03-27 21:49:24 +0800

    1

--- script.js
+++ script.js
@@ -6008,8 +6008,8 @@ Please format your response as a valid JSON array with objects having the follow
     "chinese_translation": "中文翻译",
     "word_family": [
       {
-        "examplify": "v, 举例说明",
-        "examplary": "adj, 可作榜样的",
+        "english": "examplary",
+        "chinese": "adj, 可作榜样的",
       }
     ],
     "related_phrases": [
