5a677841bf7f104ba13b9f81763c2d1290fa331a
alex <132571895+belcouact@users.noreply.github.com>
2025-04-02 08:38:05 +0800

    1

--- script.js
+++ script.js
@@ -3104,12 +3104,6 @@ function createChatInterface() {
     }
     
     console.log('Creating new chat interface elements');
-
-    // Add welcome message
-    const welcomeMessage = document.createElement('div');
-    welcomeMessage.className = 'chat-message system-message';
-    welcomeMessage.innerHTML = '你好，我是你的AI老师，有什么我可以帮你的吗？';
-    chatContainer.appendChild(welcomeMessage);
     
     // Create the chat interface
     const chatInterface = document.createElement('div');
