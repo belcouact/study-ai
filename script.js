1428e6b55ac99749cd6c547260bbf26792835b76
alex <132571895+belcouact@users.noreply.github.com>
2025-04-02 11:38:42 +0800

    1

--- script.js
+++ script.js
@@ -908,10 +908,10 @@ function displayCurrentQuestion() {
                 `;
                 
                 const resultText = isCorrect 
-                    ? `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    ? `<svg width="20" height="240" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path d="M20,6 L9,17 L4,12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
                         </svg> 正确！答案是：${correctAnswer}`
-                    : `<svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    : `<svg width="20" height="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path d="M18 6L6 18M6 6l12 12" stroke="currentColor" stroke-width="2"  stroke-linecap="round" stroke-linejoin="round"></path>
                       </svg> 错误。正确答案是：${correctAnswer}`;
                 
